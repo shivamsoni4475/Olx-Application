297ef1da680862a62e17765081161b78a8e385b7
shivamsoni4475 <48549866+shivamsoni4475@users.noreply.github.com>
2021-12-04 09:19:44 +0530

    Delete OLX-Masterdata directory

--- OLX-Masterdata/src/main/java/com/olx/repository/StatusRepository.java
+++
@@ -1,9 +0,0 @@
-package com.olx.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.olx.entity.StatusEntity;
-
-public interface StatusRepository extends JpaRepository<StatusEntity, Integer> {
-
-}
